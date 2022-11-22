b0d0c5b06b42291619a348bb0de1c386642f486e
kevinkorza <kevingkorza@gmail.com>
2022-11-22 12:26:21 -0500

    file and component creation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/style.css";
 
-function App() {
+
+export default function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Kevin Korza's Portfolio!</h1>
+      <h2>It's going to be fun!</h2>
     </div>
   );
 }
-
-export default App;
